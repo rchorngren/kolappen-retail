ec1b7e9cb6b1b9692b110deca0d13f8a4b783a9d
Robert Horngren <robert@horngren.net>
2021-01-31 13:30:26 +0100

    initial commit

---
+++ kolappen-retail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kolappen-retail
+//
+//  Created by Robert Horngren on 2021-01-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
