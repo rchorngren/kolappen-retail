1e64e9cb0dd4ae37ef3e77ce283865067daf1469
Robert Horngren <robert@horngren.net>
2021-02-02 14:41:30 +0100

    Added firebase

---
+++ Pods/Target Support Files/Pods-kolappen-retail-kolappen-retailUITests/Pods-kolappen-retail-kolappen-retailUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kolappen_retail_kolappen_retailUITests : NSObject
+@end
+@implementation PodsDummy_Pods_kolappen_retail_kolappen_retailUITests
+@end
---
+++ Pods/Target Support Files/Pods-kolappen-retailTests/Pods-kolappen-retailTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kolappen_retailTests : NSObject
+@end
+@implementation PodsDummy_Pods_kolappen_retailTests
+@end
