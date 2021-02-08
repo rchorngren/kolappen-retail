053025a8944b7405caf8a0828a644c4aaac34a95
mirandamutka <michirisu@outlook.com>
2021-02-08 14:57:31 +0100

    added visual elements and initial functionality for updating queue number

--- kolappen-retail/Views/QueueView.swift
+++ kolappen-retail/Views/QueueView.swift
@@ -9,76 +9,108 @@ import SwiftUI
 import Firebase
 
 struct QueueView: View {
-    
+
     var db = Firestore.firestore()
-    
+
     @State var uid : String = ""
     @State var shopName : String = ""
-
     @State var currentQueueNumber : Int = 0
     @State var highestQueueNumber : Int = 0
     @State var queueLength : Int = 0
     @State var documentId : String = ""
-    
+
     var body: some View {
         ZStack {
-            Color("Background")
-            VStack {
-                Spacer()
-                Text("Butik: \(shopName)")
-                    .foregroundColor(Color("Text"))
-                Spacer()
-                Text("Nu betjänas")
-                    .foregroundColor(Color("Text"))
-                Text("\(currentQueueNumber)")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .foregroundColor(Color("Text"))
-                Spacer()
-                Text("Personer i kö")
-                    .foregroundColor(Color("Text"))
-                Text("\(queueLength)")
-                    .foregroundColor(Color("Text"))
+            Text("Hello from queueview")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+
+
+            ZStack {
+                Color("Background")
+                VStack {
+                    Spacer()
+                    Text("Nu betjänas")
+                        .foregroundColor(Color("Text"))
+                    Text("\(currentQueueNumber)")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(Color("Text"))
+                    Spacer()
+                    Text("Personer i kö")
+                        .foregroundColor(Color("Text"))
+                    Text("\(queueLength)")
+                        .foregroundColor(Color("Text"))
+                        .padding(.bottom, 50)
+                    Button(action: {
+                        nextCustomer()
+                    }, label: {
+                        Text("Nästa kund")
+                        .font(.title2)
+                        .foregroundColor(Color("Text"))
+                        .padding(.horizontal)
+                    })
                     .padding(.bottom, 50)
-                Button(action: {
-                    nextCustomer()
-                }, label: {
-                    Text("Nästa kund")
-                    .font(.title2)
-                    .foregroundColor(Color("Text"))
-                    .padding(.horizontal)
-                })
-                .padding(.bottom, 50)
+
+                    HStack {
+                        NavigationLink(
+                            destination: SupportView()) {
+                                Text("Kontakta support")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(Color("Link"))
+                                    .padding(.horizontal)
+                            }
+                        NavigationLink(
+                            destination: SettingsView()) {
+                                Text("Inställningar")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(Color("Link"))
+                                    .padding(.horizontal)
+                            }
+                    }
+                    .padding(.bottom, 50)
+
+                }
             }
-        }
-        .ignoresSafeArea()
         .onAppear() {
             verifyUser()
-        }
+            .navigationBarTitle("\(shopName)")
+//            .navigationBarItems(trailing: NavigationLink(
+//                                        destination: SettingsView()) {
+//                                            Text("Inställningar")
+//                                        }
+//                ).navigationBarBackButtonHidden(true)
+            .navigationBarBackButtonHidden(true)
+            .ignoresSafeArea()
+            .onAppear() {
+                verifyUser()
+            }
+
+
     }
-    
+
     private func nextCustomer() {
         let newQueueNumber = currentQueueNumber + 1
         do {
             db.collection("users").document(documentId).updateData(["currentQueueNumber" : newQueueNumber])
         }
     }
-    
+
     private func verifyUser() {
         if let user = Auth.auth().currentUser {
+            let uid = user.uid
             uid = user.uid
             let email = user.email
             print("loggedin user: \(uid) \(email!)")
-            
+
             db.collection("users").whereField("uid", isEqualTo: uid).addSnapshotListener() { (snapshot, error) in
                 if let error = error {
                     print("there was an error regarding snapshot: \(error)")
                 } else {
                     for document in snapshot!.documents {
-                        
+
                         let result = Result {
                             try document.data(as: Shop.self)
                         }
-                        
+
                         switch result {
                             case .success(let shop):
                                 if let shop = shop {
@@ -93,16 +125,22 @@ struct QueueView: View {
                                 case.failure(let error):
                                     print("Error decoding item: \(error)")
                         }
-                        
+
                     }
                 }
             }
         }
     }
-    
+
 }
 
 
+struct QueueView_Previews: PreviewProvider {
+    static var previews: some View {
+        QueueView()
+            .environment(\.colorScheme, .dark)
+    }
+}
 //struct QueueView_Previews: PreviewProvider {
 //    static var previews: some View {
 //        QueueView()
