db1ce0f9cf9fafd097895332173bfeb187bfebf8
mirandamutka <michirisu@outlook.com>
2021-02-08 14:57:31 +0100

    added visual elements and initial functionality for updating queue number

--- kolappen-retail/Views/QueueView.swift
+++ kolappen-retail/Views/QueueView.swift
@@ -14,15 +14,52 @@ struct QueueView: View {
     
     @State var uid : String = ""
     @State var shopName : String = ""
+    @State var currentQueueNumber : Int = 0
+    @State var highestQueueNumber : Int = 0
+    @State var queueLength : Int = 0
+    @State var documentId : String = ""
     
     var body: some View {
         ZStack {
-            Text("Butik: \(shopName)")
-                .foregroundColor(Color("Text"))
-                
+            Color("Background")
+            VStack {
+                Spacer()
+                Text("Butik: \(shopName)")
+                    .foregroundColor(Color("Text"))
+                Spacer()
+                Text("Nu betjänas")
+                    .foregroundColor(Color("Text"))
+                Text("\(currentQueueNumber)")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color("Text"))
+                Spacer()
+                Text("Personer i kö")
+                    .foregroundColor(Color("Text"))
+                Text("\(queueLength)")
+                    .foregroundColor(Color("Text"))
+                    .padding(.bottom, 50)
+                Button(action: {
+                    nextCustomer()
+                }, label: {
+                    Text("Nästa kund")
+                    .font(.title2)
+                    .foregroundColor(Color("Text"))
+                    .padding(.horizontal)
+                })
+                .padding(.bottom, 50)
             }
+        }
+        .ignoresSafeArea()
         .onAppear() {
             verifyUser()
+        }
+    }
+    
+    private func nextCustomer() {
+        let newQueueNumber = currentQueueNumber + 1
+        do {
+            _ = try db.collection("users").document(documentId).updateData(["currentQueueNumber" : newQueueNumber]
+            )
         }
     }
     
@@ -46,7 +83,20 @@ struct QueueView: View {
                     else {
                         return
                     }
+                    guard let currentQueueDB = dataDescription?["currentQueueNumber"]
+                    else {
+                        return
+                    }
+                    guard let highestQueueDB = dataDescription?["highestQueueNumber"]
+                    else {
+                        return
+                    }
                     shopName = "\(shopNameDB)"
+                    currentQueueNumber = currentQueueDB as! Int
+                    highestQueueNumber = highestQueueDB as! Int
+                    queueLength = highestQueueNumber - currentQueueNumber
+                    documentId = document!.documentID
+                    
                 }
             }
 //            docRef.getDocument { (document, error) in
@@ -62,9 +112,9 @@ struct QueueView: View {
 }
 
 
-struct QueueView_Previews: PreviewProvider {
-    static var previews: some View {
-        QueueView()
-            .environment(\.colorScheme, .dark)
-    }
-}
+//struct QueueView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QueueView()
+//            .environment(\.colorScheme, .dark)
+//    }
+//}
