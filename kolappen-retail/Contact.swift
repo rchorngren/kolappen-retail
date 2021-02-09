a108d35dd60654def6c6647d8a5cabe1f356e693
Emmeline Mutka <emmeline.mutka@iths.se>
2021-02-09 14:30:41 +0100

    Added contact information from Firebase

---
+++ kolappen-retail/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  kolappen-retail
+//
+//  Created by Emmeline Mutka on 2021-02-09.
+//
+
+import Foundation
+struct Contact : Codable {
+
+var phone: String
+var email: String
+    
+}
---
+++ kolappen-retail/RetailContactInformation.swift
@@ -0,0 +1,8 @@
+//
+//  RetailContactInformation.swift
+//  kolappen-retail
+//
+//  Created by Emmeline Mutka on 2021-02-08.
+//
+
+import Foundation
