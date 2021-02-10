35b8808874edadb1c6a5257c2e41b5935ef085e1
Robert Horngren <robert@horngren.net>
2021-02-10 10:38:28 +0100

    wip - added SettingsView

---
+++ kolappen-retail/Views/SettingsView.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsView.swift
+//  kolappen-retail
+//
+//  Created by Robert Horngren on 2021-02-10.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct SettingsView: View {
+
+    let db = Firestore.firestore()
+    
+    var body: some View {
+        ZStack {
+            Color("Background")
+            VStack {
+                Text("SettingsView")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(Color("Text"))
+                    .padding(.top, 100)
+            }
+                
+        }
+        .ignoresSafeArea()
+        .onAppear() {
+            print("settingsView")
+        }
+    }
+    
+    
+    
+}
+
+
+
+//struct SupportView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SupportView()
+//            .environment(\.colorScheme, .dark)
+//    }
+//}
